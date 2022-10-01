71097bd3d719d5908089490e203ce8a1a8d118e8
Thigo <thiago.l17@yahoo.com>
2022-10-01 13:40:44 -0300

    done!

--- src/index.js
+++ src/index.js
@@ -10,19 +10,73 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  
+  const { username } = request.headers;
+
+  const user = users.find(user => username === user.username);
+
+  if(!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
+  const { user } = request;
   
+  const userHasPro = user.pro 
+  const todosAmount = user.todos.length
+  
+
+  if (!userHasPro && todosAmount >= 10) {
+    return response.status(403).json({ error: 'Maximum of todo created' });
+  }
+
+  next()
 }
 
 function checksTodoExists(request, response, next) {
-  
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({ error:'User not found' })
+  }
+
+  const isUuid = validate(id)
+
+  if(!isUuid) {
+    return response.status(400).json({ error: 'UUID not valide' })
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo) {
+    return response.status(404).json({ error: 'Todo not found' })
+  }
+
+  request.todo = todo
+  request.user = user
+
+  next()
 }
 
 function findUserById(request, response, next) {
+  const { id } = request.params
   
+  const user = users.find(user => user.id === id)
+
+  if(!user) {
+    return response.status(404).json({ error: 'User not exists' })
+  } 
+
+  request.user = user
+
+  next()
 }
 
 app.post('/users', (request, response) => {
